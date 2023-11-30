02de5c8f204c125e4393513cd6e74f6141b8af84
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-11-30 19:26:31 +0200

    CODE: users can now register

--- app.js
+++ app.js
@@ -6,22 +6,19 @@ const passport = require('passport')
 const mongoose = require('mongoose')
 const config = require('./config/database')
 
+// Connect to database
 mongoose.connect(config.database, { useNewUrlParser: true , useUnifiedTopology: true })
-
+// on connect
 mongoose.connection.on('connected', ()=>{
     console.log('Connected to database: '+config.database)
 })
+//on error
 mongoose.connection.on('error', (err)=>{
     console.log('Database error: '+err)
 })
 
 const app = express()
-
 const user  = require('./routes/users')
-
-
-
-
 const port = 3000
 
 // cors middleware
@@ -35,14 +32,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 
-//indev route
+//index route
 app.get('/', (req,res)=>{
     res.send("Invalid Endpoint.")
 })
 
 app.use('/users', user)
-
-
 
 
 
