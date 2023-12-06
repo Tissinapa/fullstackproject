d92199b4f7f7c5fb51b84afb57ac1f280ebf0b83
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-12-06 11:59:18 +0200

    CODE: aded login and authentication

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyparser = require('body-parser')
 const cors = require('cors')
 const passport = require('passport')
 const mongoose = require('mongoose')
+const session = require('express-session')
 const config = require('./config/database')
 
 // Connect to database
@@ -26,6 +27,20 @@ app.use(cors())
 
 //Bodyparser middleware
 app.use(bodyparser.json())
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./config/passport')(passport)
+
 
 // Set static folder 
 app.use(express.static(path.join(__dirname, 'public')))
